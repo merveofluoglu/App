3b6b982f37ae6afdaee721b9cf3fb8c96ca8ade7
Ömer Cem Tabar <oemercem.tabar@studenti.unipd.it>
2023-03-30 21:58:19 +0200

    User,Role Modals and AbstractDao

---
+++ src/main/java/dao/user/CreateUserDAO.java
@@ -0,0 +1,4 @@
+package dao.user;
+import org.json.JSONObject;
+public class CreateUserDAO {
+}
--- src/main/java/resource/Role.java
+++ src/main/java/resource/Role.java
@@ -1,4 +1,37 @@
 package resource;
-
+import java.sql.Timestamp;
+import java.util.Objects;
 public class Role {
+    private Long role_id;
+
+    private String name;
+
+    public Role(Long role_id, String name) {
+        this.role_id = role_id;
+        this.name = name;
+    }
+
+    public Long getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(Long role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "role_id='" + role_id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
