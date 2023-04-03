20ad453181ccbbde59a1dfaadeb576a46a33f89b
mehmetsan <sanisoglumehmet@gmail.com>
2023-04-04 00:30:16 +0200

    Message attributes created

--- src/main/java/resource/Message.java
+++ src/main/java/resource/Message.java
@@ -1,4 +1,18 @@
 package resource;
 
+import java.sql.Timestamp;
+
 public class Message {
+
+    private long message_id;
+    private long creator_id;
+    private long recipient_id;
+    private long parent_message_id;
+    private String subject;
+    private String message_body;
+    private Timestamp creation_date;
+    private Timestamp expiration_date;
+    private boolean is_read;
+
+
 }
