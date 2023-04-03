3e9b4e51dadb325555b9968cfd77bd14af97dfaf
dedeha39 <dehakose@hotmail.com>
2023-04-03 16:48:44 +0200

    Category Dao

---
+++ src/main/java/dao/Category/CreateCategoryDao.java
@@ -0,0 +1,39 @@
+package dao.Category;
+import resource.Category;
+import dao.AbstractDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CreateCategoryDao extends AbstractDAO {
+    // SQL Query to be executed : CREATE categoryobject in database.
+
+    private static  final String STATEMENT = "INSERT INTO category (category_name) VALUES (?)";
+
+    protected CreateCategoryDao(Connection con) {super(con);}
+    //Declare and set up a connection in constructor
+    @Override
+    protected void doAccess() throws Exception {}
+
+    public String CreateCategory(Category category) throws SQLException {
+        PreparedStatement pstmt = null;
+        int rs;
+        try {
+            //Prepare and set the statement object to be executed
+            pstmt = con.prepareStatement(STATEMENT);
+            pstmt.setString(1, category.getCategory_name());
+
+            rs = pstmt.executeUpdate();
+            if (rs != 1) {
+                throw new SQLException("Category Creation Failed!");
+            }
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            con.close();
+        }
+        return "Category Creation Successfully!";
+    }
+}
