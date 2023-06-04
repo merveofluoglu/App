3cf647e130f982933163bdf1dcf207d9babf36da
Anıl Özfırat <anilozfirat@gmail.com>
2023-06-04 23:54:09 +0200

    GoogleLoginServlet

--- src/main/java/servlet/GoogleLoginServlet.java
+++ src/main/java/servlet/GoogleLoginServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class GoogleLoginServlet extends HttpServlet {
+public class GoogleLoginServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
